d31494b34b561ed33635988b473766c8cf1d088c
7VREW <129406618+7VREW@users.noreply.github.com>
2023-10-26 16:21:24 +0200

    Update FloatingButton

--- PlayPop/Composants/FloatingButton.swift
+++ PlayPop/Composants/FloatingButton.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct FloatingButton: View {
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 200, height: 60)
-                .foregroundStyle(.regularMaterial)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 30)
-                        .stroke(LinearGradient(colors: [Color(.systemOrange), Color(.systemYellow)], startPoint: .leading, endPoint: .trailing), lineWidth: 3)
-            )
-            Text("Valider")
+    
+    struct CustomButtonAnimation: ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .scaleEffect(configuration.isPressed ? 1.2 : 1)
+                .animation(.bouncy(duration: 0.2, extraBounce: 0.2), value: configuration.isPressed)
         }
+    }
+    
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 200, height: 60)
+                    .foregroundStyle(.regularMaterial)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(LinearGradient(colors: [Color(.systemOrange), Color(.systemYellow)], startPoint: .leading, endPoint: .trailing), lineWidth: 3)
+                )
+                Text("Valider")
+            }
+        })
+        .buttonStyle(CustomButtonAnimation())
     }
 }
 
